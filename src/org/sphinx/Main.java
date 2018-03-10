3949418369ba7c2f15c65bb58ecf880a83074cb3
Mathhman <minecraftmathhman@gmail.com>
2018-03-10 17:53:35 -0500

    Added a few basic FXML files.

--- src/org/sphinx/Main.java
+++ src/org/sphinx/Main.java
@@ -1,9 +1,27 @@
 package org.sphinx;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 3/10/2018
  */
-public class Main {
+public class Main extends Application {
+    private final URL menuURL = getClass().getResource("/Menu.fxml");
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }

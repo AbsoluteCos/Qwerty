50d613a47f72cbaafa5615f09724d548ca67631f
Mathhman <minecraftmathhman@gmail.com>
2018-03-11 00:32:06 -0500

    Added some Display edits

--- src/main/java/org/sphinx/controller/DisplayController.java
+++ src/main/java/org/sphinx/controller/DisplayController.java
@@ -1,9 +1,23 @@
 package org.sphinx.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * @author SirMathhman
  * @version 0.0.0
  * @since 3/10/2018
  */
-public class DisplayController {
+public class DisplayController implements Initializable {
+    @FXML
+    private AnchorPane graph;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }

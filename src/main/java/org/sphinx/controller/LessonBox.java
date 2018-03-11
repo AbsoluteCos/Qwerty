2e75809f5995886e835fc8544256650082b980b4
Mathhman <minecraftmathhman@gmail.com>
2018-03-11 05:39:10 -0400

    Some minor fixes, a blueprint for some implementations to do.

--- src/main/java/org/sphinx/controller/LessonBox.java
+++ src/main/java/org/sphinx/controller/LessonBox.java
@@ -1,8 +1,17 @@
 package org.sphinx.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TitledPane;
+import javafx.stage.Stage;
+import org.sphinx.FXMLBundle;
 import org.sphinx.Lesson;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+
+import static org.sphinx.Main.instance;
 
 /**
  * @author SirMathhman
@@ -13,7 +22,32 @@ public class LessonBox {
     @FXML
     private TitledPane namePane;
 
+    private final URL lessonPaneURL = getClass().getResource("/LessonPane.fxml");
+
+    @FXML
+    private TextArea descriptionArea;
+
+    private Lesson lesson;
+
+    @FXML
+    public void start() {
+        try {
+            FXMLBundle bundle = new FXMLBundle(lessonPaneURL);
+            Stage stage = Controller.toStage(bundle);
+            stage.show();
+
+            Object controller = bundle.getController();
+            if (controller instanceof LessonController) {
+                ((LessonController) controller).load(lesson);
+            }
+        } catch (IOException e) {
+            instance.getConsole().log(Level.WARNING, e);
+        }
+    }
+
     public void setLesson(Lesson lesson){
-        namePane.setText(lesson.getName());
+        this.namePane.setText(lesson.getName());
+
+        this.lesson = lesson;
     }
 }

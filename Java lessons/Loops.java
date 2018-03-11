0752e255811912949d01c0c88cb9494751dedc76
Sahan Metpallhy <sahan.metpally@gmail.com>
2018-03-10 21:33:18 -0500

    Lesson 3 and Lesson 3's .java

---
+++ Java lessons/Loops.java
@@ -0,0 +1,17 @@
+/*Exercise 3 - learning to make for loops, while loops and if statements. 
+ * Objective - be able to decide which loop to use and use it correctly.
+ * Make loops that do certain tasks.
+ * */
+package exercises;
+public class Loops {
+	int i = 94;
+//write a main method.
+//after writing main method, write a for loop that counts from 0 to 100 and prints out each number. Make a while loop that runs as long as i > 57.
+//Then, write an if statement to check if the variable used in the for loop is equal to i. If it is, the print success. 
+//Then create an else if to check if i > the for loop variable. If it is, print greater than.
+//After the else if, make an else statement that prints fail.
+}
+/*Extension - 
+Knowing what you have learned so far, make a new class named LoopsPlus that does something unexpected and spectacular. Use for and while loops. 
+Extra - you can have nested loops, which is when there is a loop inside a loop. Try this out and see how it goes.
+*/

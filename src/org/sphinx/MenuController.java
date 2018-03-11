aa7834039d87646f0a09b10439a5cde99e371e8e
Mathhman <minecraftmathhman@gmail.com>
2018-03-10 21:12:48 -0500

    Configured Menu for creating Display

--- src/org/sphinx/MenuController.java
+++ src/org/sphinx/MenuController.java
@@ -1,13 +1,16 @@
 package org.sphinx;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ResourceBundle;
 import java.util.logging.Level;
 
+import static org.sphinx.Main.*;
 import static org.sphinx.Main.instance;
 
 /**
@@ -15,11 +18,21 @@ import static org.sphinx.Main.instance;
  * @version 0.0.0
  * @since 3/10/2018
  */
-public class MenuController extends Controller {
+public class MenuController extends Controller implements Initializable {
     private final URL settingsLocation = getClass().getResource("/Settings.fxml");
+    private final URL displayLocation = getClass().getResource("/Display.fxml");
 
     @FXML
     private Text versionText;
+
+    @FXML
+    public void resumeACourse(){
+        try {
+            stage.setScene(Controller.toScene(new FXMLBundle(displayLocation)));
+        } catch (IOException e) {
+            instance.getConsole().log(Level.SEVERE, e);
+        }
+    }
 
     @FXML
     public void openSettings() {
@@ -29,5 +42,10 @@ public class MenuController extends Controller {
         } catch (IOException e) {
             instance.getConsole().log(Level.SEVERE, e);
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        versionText.setText(instance.getProperties().getProperty("version", "0.0"));
     }
 }

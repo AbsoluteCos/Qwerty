87bb186af4d992b191c1ae9f58782a775766d9c5
Mathhman <minecraftmathhman@gmail.com>
2018-03-11 00:41:20 -0500

    Configured actions regarding courses in the display

--- src/main/java/org/sphinx/controller/DisplayController.java
+++ src/main/java/org/sphinx/controller/DisplayController.java
@@ -2,6 +2,7 @@ package org.sphinx.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
@@ -15,6 +16,19 @@ import java.util.ResourceBundle;
 public class DisplayController implements Initializable {
     @FXML
     private AnchorPane graph;
+
+    @FXML
+    private ChoiceBox<String> courseOptions;
+
+    @FXML
+    public void addCourse(){
+
+    }
+
+    @FXML
+    public void removeCourse(){
+
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {

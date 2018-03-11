fdfc633f34afabe87a8f76ce27aa45e6dd8ad2d1
Sahan Metpallhy <sahan.metpally@gmail.com>
2018-03-10 20:59:04 -0500

    Adding Lesson 1, Lesson 2, and Lesson2's class

---
+++ Java lessons/CreateClass.java
@@ -0,0 +1,12 @@
+/*Exercise 2 - learning to make classes and main method. 
+ * Objective - be able to create new classes with a main method.
+ * Make practice classes which will do specific tasks.
+ * */
+package exercises;
+public class CreateClass {
+//write a main method. Refer to lesson explanation for syntax.
+//after writing main method, write code to add two numbers. 
+}
+/*Extension - 
+Knowing what you have learned so far, make a new class named myClass that prints the address of the white house.
+*/
